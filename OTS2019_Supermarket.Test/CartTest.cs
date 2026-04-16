0ccf37d20b849d77679196f37ab16a7548323b08
teodorgavric <gavric.teo05@gmail.com>
2026-04-16 19:50:00 +0200

    Vezbe 5

--- OTS2019_Supermarket.Test/CartTest.cs
+++ OTS2019_Supermarket.Test/CartTest.cs
@@ -8,8 +8,22 @@ using System.Threading.Tasks;
 
 namespace OTS_Supermarket.Test
 {
+    [TestFixture]
     public class CartTest
     {
+        [Test]
+        public void AddOneToCart_ShouldAddItemToCart_Success()
+        {
+            // ARRANGE
+            Cart cart = new Cart();
+            Monitor monitor = new Monitor();
 
+            //ACT
+            cart.AddOneToCart(monitor);
+
+            //ASSERT
+            Assert.That(cart.Size, Is.EqualTo(1));
+            Assert.That(cart.Amount, Is.EqualTo(100));
+        }
     }
 }
